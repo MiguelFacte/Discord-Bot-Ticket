48c5a35e0bf44ce92935b5f1d2b20c5480d60ffe
Nil PM <92615321+MiguelFacte@users.noreply.github.com>
2023-02-21 22:47:23 -0600

    Update ping.js

--- Discord Bot tickets/slashcmd/ping.js
+++ Discord Bot tickets/slashcmd/ping.js
@@ -1,15 +1,19 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
-const Discord = require("discord.js")
+
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Shows the bot latency."),
-    async run(client, interaction){
+        .setName("ping")
+        .setDescription("Shows the bot latency."),
+        
+    async run(client, interaction) {
         const embed = new MessageEmbed()
-			.setColor("0x3EFE00")
-			.setTitle('Pong 🏓')
-			.setDescription("🟢 "+ Math.floor(client.ws.ping)+"ms");
-		await interaction.reply({ ephemeral: true, embeds: [embed] })
+            .setColor("0x3EFE00")
+            .setTitle('Pong 🏓')
+            .setDescription(`🟢 ${Math.floor(client.ws.ping)} ms`);
+        return await interaction.reply({ 
+            ephemeral: true, 
+            embeds: [embed] 
+        })
     }
 }
